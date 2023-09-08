5acfad7e5b846643180830b63f9daeba68d8090d
notharshprajapati <notharsh0243@gmail.com>
2023-09-08 13:39:11 +0530

    Responsive Navbar done

---
+++ src/Components/ThemeToggle/ThemeToggle.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./ThemeToggle.module.css";
+
+const ThemeToggle = () => {
+  return <div className={styles.container}>TT</div>;
+};
+
+export default ThemeToggle;
