aaf408eab9fb2bff7c077372c775ff7a807474e4
notharshprajapati <notharsh0243@gmail.com>
2023-09-09 13:45:40 +0530

    hamburger menu done

--- src/Components/authLinks/AuthLinks.jsx
+++ src/Components/authLinks/AuthLinks.jsx
@@ -1,19 +1,44 @@
-import React from "react";
-import styles from "./authLinks.module.css";
+"use client";
 import Link from "next/link";
+import styles from "./authLinks.module.css";
+import { useState } from "react";
 
 const AuthLinks = () => {
-  //temp
-  const status = "notauthenticated";
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      {status === "notauthenticated" ? (
-        <Link href="/login">Login</Link>
+      {status === "unauthenticated" ? (
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
       ) : (
         <>
-          <Link href="/write">Write</Link>
+          <Link href="/write" className={styles.link}>
+            Write
+          </Link>
           <span className={styles.link}>Logout</span>
         </>
+      )}
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Homepage</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+          {status === "notauthenticated" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span className={styles.link}>Logout</span>
+            </>
+          )}
+        </div>
       )}
     </>
   );
